3c1fc629ab7e2720e0d1556a5b4736584ce9e7a0
kashwa <kashwa80@gmail.com>
2018-10-28 16:30:45 +0200

    Push First Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// TODO: type your JS code here.
+
+let btn = document.getElementById('users');
+
+btn.addEventListener('click', function(){
+    let req = new XMLHttpRequest();
+    req.onreadystatechange = function ()
+    {
+        if (req.readyState == req.DONE)
+        {
+            let results = JSON.parse(req.response);
+
+            for (let i = 0, j =results.results.length; i<j; i++)
+            {
+                console.log("User no: " + (i+2));
+                console.log("Name : " + results.results[i].name.first + ' ' + results.results[i].name.last);
+            }
+        }
+    }
+    req.onerror = function()
+    {
+        console.log(new Error("error yabn el wes5a...!"));
+    }
+    req.open('GET', 'https://randomuser.me/api?results=1');
+    req.send();
+}, false);
