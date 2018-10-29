faac92fe97d0b52fa7015346255c2e4cb49004f2
kashwa <kashwa80@gmail.com>
2018-10-29 15:33:22 +0200

    apply innerHTML logic

--- index.js
+++ index.js
@@ -8,19 +8,31 @@ btn.addEventListener('click', function(){
     {
         if (req.readyState == req.DONE)
         {
+            let usersContainer = document.querySelector("div.users");
+            let usersHTML = '';
             let results = JSON.parse(req.response);
 
             for (let i = 0, j =results.results.length; i<j; i++)
             {
-                console.log("User no: " + (i+2));
-                console.log("Name : " + results.results[i].name.first + ' ' + results.results[i].name.last);
+                // console.log("User no: " + (i+2));
+                // console.log("Name : " + results.results[i].name.first + ' ' + results.results[i].name.last);
+                // console.log(results.results[i]);
+
+                usersHTML += '<div class="user">\n' +
+                    '\n' +
+                             '<h3>User number ' +(i+1)+ '</h3>' +
+                             '<p>Name <span>' +results.results[i].name.first+ ' '+results.results[i].name.last+ '</span></p>' +
+                             '<p>Gender: <strong>'+ results.results[i].gender +'</strong> </p>' +
+                             '<p>Address: <span>'+ results.results[i].location.street +'</span></p>'
+                             +'</div>'
             }
+            usersContainer.innerHTML = usersHTML;
         }
     }
     req.onerror = function()
     {
         console.log(new Error("Some errors happened here buddy!"));
     }
-    req.open('GET', 'https://randomuser.me/api?results=1');
+    req.open('GET', 'https://randomuser.me/api?results=7');
     req.send();
 }, false);
